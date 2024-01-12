0ae23c2cb96a09d6b3e8c089a7f2e8270216938f
Rostislav Kupriyanov <83307529+njiaaah@users.noreply.github.com>
2024-01-12 23:43:10 +0300

    allformats

--- script/swipers.js
+++ script/swipers.js
@@ -26,7 +26,36 @@ const swiperAdvantages = new Swiper('.section-advantages-swiper', {
 
   });
 
+const swiperAllformats = new Swiper('.section-allformats-swiper', {
+    slidesPerView: "auto",    
+
+    breakpoints: {
+        1000: {
+            spaceBetween: 34,
+        },
+        560: {
+            spaceBetween: 16,
+        },
+        320: {
+            spaceBetween: 16,
+        },
+    },
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+
+      scrollbar: {
+        el: '.swiper-scrollbar',
+        hide: false,
+        draggable: true,
+      },
+
+  });
+
 const swiperPromo = new Swiper('.section-promo-swiper', {
+    
     slidesPerView: "auto",    
 
     breakpoints: {

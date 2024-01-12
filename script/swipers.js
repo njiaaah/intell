d355510690214d4850cb0623b260d54c525815b5
Timur Albotov <sweciti@rambler.ru>
2024-01-12 11:19:31 +0300

    promo complete?

--- script/swipers.js
+++ script/swipers.js
@@ -26,5 +26,33 @@ const swiperAdvantages = new Swiper('.section-advantages-swiper', {
 
   });
 
+const swiperPromo = new Swiper('.section-promo-swiper', {
+    slidesPerView: "auto",    
+
+    breakpoints: {
+        1000: {
+            spaceBetween: 34,
+        },
+        560: {
+            spaceBetween: 16,
+        },
+        320: {
+            spaceBetween: 8,
+        },
+    },
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+
+      scrollbar: {
+        el: '.swiper-scrollbar',
+        hide: false,
+        draggable: true,
+      },
+
+  });
+
 
 
